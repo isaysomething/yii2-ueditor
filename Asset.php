519cabd263f41d8011bf40af093fa5c3b09c97fe
yrs <yrsemail@163.com>
2014-06-28 02:35:26 +0800

    Create Asset.php

---
+++ Asset.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace yrs\ueditor;
+
+use Yii;
+use yii\web\AssetBundle;
+use yii\web\View;
+
+class Asset extends AssetBundle
+{
+    public $jsOptions = ['position' => View::POS_HEAD];
+    public $sourcePath = '@vendor/yrssoft/yrs-ueditor/assets/';
+    public $js = [
+        'ueditor.config.js',
+        'ueditor.all.js',
+    ];
+
+    public $css = [
+        'themes/default/default.css',
+    ];
+}
